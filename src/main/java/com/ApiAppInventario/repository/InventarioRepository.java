f8e9ee5e28b72d550fa512d248f1626dbb6e5d1a
BenyiUriel <benyi.uriel2006@gmail.com>
2025-07-10 09:23:51 -0600

    Se crea el repository para el inventario

---
+++ src/main/java/com/ApiAppInventario/repository/InventarioRepository.java
@@ -0,0 +1,9 @@
+package com.ApiAppInventario.repository;
+
+import com.ApiAppInventario.model.Inventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<Inventario,Long> {
+}
