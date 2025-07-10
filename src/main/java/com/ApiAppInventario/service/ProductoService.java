4f9fb2f1c1d56ae4ac397170092988c7a901f1a5
BenyiUriel <benyi.uriel2006@gmail.com>
2025-07-10 10:11:44 -0600

    Se crea el servicio para producto y sus metodos

---
+++ src/main/java/com/ApiAppInventario/service/ProductoService.java
@@ -0,0 +1,15 @@
+package com.ApiAppInventario.service;
+
+import com.ApiAppInventario.model.Producto;
+
+import java.util.List;
+
+public interface ProductoService {
+
+    void crearProducto(Producto producto);
+    void deleteProducto(Producto producto);
+    void updateProducto(Long id,Producto producto);
+    List<Producto> findAllProductos();
+    Producto findProductoById(Long id_producto);
+
+}
